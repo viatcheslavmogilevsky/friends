fd4d58c44291b2bc00adaf646308e269cd98e103
viatcheslavmogilevsky <keithflint@inbox.ru>
2012-07-18 03:14:30 +0400

    content_tag call instead html string concatenation

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,14 @@
 module UsersHelper
 	def get_links(user)
 		if current_user.friend?(user)
-			"<li>"+link_to("Delete from friends", delete_user_path(user), :method => :delete)+"</li>"
+			content_tag(:li,link_to("Delete from friends",delete_user_path(user), :method => :delete))
 		elsif var = current_user.target_user?(user)
-			"<li>"+link_to("Cancel invite", cancel_user_friendship_path(var), :method => :delete)+"</li>" 
+			content_tag(:li,link_to("Cancel invite", cancel_user_friendship_path(var), :method => :delete))
 		elsif var = current_user.candidate?(user)
-			"<li>"+link_to("Accept invite", accept_user_friendship_path(var), :method => :put)+"</li>"+
-			"<li>"+link_to("Cancel invite", cancel_user_friendship_path(var), :method => :delete) +"</li>"
+			content_tag(:li,link_to("Accept invite", accept_user_friendship_path(var), :method => :put)) +
+			content_tag(:li,link_to("Cancel invite", cancel_user_friendship_path(var), :method => :delete))
 		else
-			"<li>"+link_to("Add to friends", create_invite_user_path(user), :method => :post)+"</li>"
+			content_tag(:li,link_to("Add to friends", create_invite_user_path(user), :method => :post))
 		end	
 	end
 end
